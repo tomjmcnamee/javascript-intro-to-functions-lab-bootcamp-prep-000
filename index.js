bcc1284e2c6926be83a00bf11dcec4c9cd01631f
Tom McNamee <tom@tomjmcnamee.com>
2019-02-27 01:58:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,13 +22,12 @@ function sayHiToGrandma(string) {
 }
 
 function sayHiToGrandma(string) {
-  if ( string === string.toUpperCase() ) {
-    return "YES INDEED!";
-  }
-}
-
-function sayHiToGrandma(string) {
   if (string === "I love you, Grandma.") {
     return "I love you, too."
+  }
+}
+function sayHiToGrandma(string) {
+  if ( string === string.toUpperCase() ) {
+    return "YES INDEED!";
   }
 }
